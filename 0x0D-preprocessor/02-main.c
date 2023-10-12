5915a664bd237b790b3c1917e2e21720f8c06ea4
mennas18 <mennasmoustafa18@gmail.com>
2023-10-11 18:01:26 -0700

    0x0D-preprocessor/

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main _ print name of the source file
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+        printf("%s\n", __FILE__);
+        return (0);
+}
